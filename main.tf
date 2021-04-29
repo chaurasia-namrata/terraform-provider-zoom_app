7aa6d57c504d53ed4a478c0258b5434d9cd75733
Namrata Chaurasia <50015194+chaurasia-namrata@users.noreply.github.com>
2021-04-29 23:34:27 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    zoom = {
+      version = "0.2"
+      source  = "hashicorp.com/edu/zoom"
+    }
+  }
+}
+
+provider "zoom" {
+  address = "https://api.zoom.us/v2/users"
+  token   = "[access_token]"
+}
+
+
+resource "zoom_user" "user1" {
+   email      = "USER_MAIL@gmail.com"
+   first_name = "[FIRSTNAME]"
+   last_name  = "[LASTNAME]"
+   status = "activate"
+}
+
+data "zoom_user" "user1" {
+   id = "USER_MAIL@gmail.com"
+}
+
+
+output "user3" {
+   value = data.zoom_user.user1
+ }
