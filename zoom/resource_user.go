81fbb3cf206193b15d3e9feabdf4b29c004afd67
Namrata Chaurasia <50015194+chaurasia-namrata@users.noreply.github.com>
2021-04-29 23:29:36 +0530

    Update resource_user.go

--- zoom/resource_user.go
+++ zoom/resource_user.go
@@ -6,8 +6,8 @@ import (
 	"log"
 	"regexp"
 	"strings"
-	"terraform-provider-zoom/client"
-	"terraform-provider-zoom/server"
+	"terraform-provider-zoom_app/client"
+	"terraform-provider-zoom_app/server"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
